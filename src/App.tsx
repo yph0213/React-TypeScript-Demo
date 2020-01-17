b9bb2976b1ec0dc4bc93277ff9e78d77c8e84e73
pengHui <yph2352111566@hotmail.com>
2020-01-17 12:11:33 +0800

    tsconfig.json文件添加注释，app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// FC = Functional Component
+// 功能组件
+// SFC = Stateless Functional Component (已弃用)
+// 无状态功能组件(已弃用)
 
 const App: React.FC = () => {
   return (
@@ -21,6 +26,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
